93cd388363a22881bfb2362706ad4d8f8679ac6a
RAGOT Cyrian <cyrian.ragot@etu.inp-n7.fr>
2023-11-20 01:53:27 +0100

    mod. revision tp01

--- Mod/TP01/tp1.v
+++ Mod/TP01/tp1.v
@@ -8,36 +8,79 @@ Section LogiqueProposition.
 Variable A B C E Y R : Prop. 
 
 Theorem Thm_0 : A /\ B -> B /\ A.
-I_imp HAetB.
+I_imp H.
 I_et.
-E_et_d A.
-Hyp HAetB.
-E_et_g B.
-Hyp HAetB.
+E_et_d(A).
+Hyp H.
+E_et_g(B).
+Hyp H.
 Qed.
 
 Theorem Thm_1 : ((A \/ B) -> C) -> (B -> C).
-(* A COMPLETER *)
+I_imp H1.
+I_imp H2.
+E_imp(A\/B).
+Hyp H1.
+I_ou_d.
+Hyp H2.
 Qed.
 
 Theorem Thm_2 : A -> ~~A.
-(* A COMPLETER *)
+I_imp H.
+I_non H1.
+I_antiT(A).
+Hyp H.
+Hyp H1.
 Qed.
 
 Theorem Thm_3 : (A -> B) -> (~B -> ~A).
-(* A COMPLETER *)
+I_imp H1.
+I_imp H2.
+I_non H3.
+I_antiT(B).
+E_imp(A).
+Hyp H1.
+Hyp H3.
+Hyp H2.
 Qed.
 
 Theorem Thm_4 : (~~A) -> A.
-(* A COMPLETER *)
+I_imp H1.
+absurde H2.
+I_antiT(A).
+E_antiT.
+I_antiT(~A).
+Hyp H2.
+Hyp H1.
+Hyp H2.
 Qed.
 
 Theorem Thm_5 : (~B -> ~A) -> (A -> B).
-(* A COMPLETER *)
+I_imp H1.
+I_imp H2.
+absurde H3.
+E_non(A).
+Hyp H2.
+E_imp(~B).
+Hyp H1.
+Hyp H3.
 Qed.
 
 Theorem Thm_6 : ((E -> (Y \/ R)) /\ (Y -> R)) -> ~R -> ~E.
-(* A COMPLETER *)
+I_imp H1.
+I_imp H2.
+I_non H3.
+E_non(R).
+E_ou(Y)(R).
+E_imp(E).
+E_et_g(Y -> R).
+Hyp H1.
+Hyp H3.
+E_et_d(E -> Y \/ R).
+Hyp H1.
+I_imp H4.
+Hyp H4.
+Hyp H2.
 Qed.
 
 (* Version en Coq *)
@@ -52,7 +95,11 @@ Qed.
 
 
 Theorem Coq_E_imp : ((A -> B) /\ A) -> B.
-(* A COMPLETER *)
+intros.
+destruct H.
+cut(A).
+Hyp H.
+Hyp H0.
 Qed.
 
 Theorem Coq_E_et_g : (A /\ B) -> A.
